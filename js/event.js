c68f4abf1f33443303eb528cec1b0eb32414bc5c
JosueAcosta <josueacosta@IsidroskBookPro.home>
2017-09-01 23:34:07 -0400

    Modified file to remove events that needed to be handled by knockout such as getting request from NYT api.

--- js/event.js
+++ js/event.js
@@ -1,34 +1,18 @@
-//triggers marker click event through data attribute 
-$("body").on("click", "[data-marker-index]", function(e){
+$("body").on("click", '[data-type="listing"]', function(e){
     e.preventDefault();
-      var index = $(this).data("marker-index");
+      var index = $(this).find('[data-type="marker-index"]').text();
       console.info('Triggering ' + markers[index].title + ' markers click event');
       google.maps.event.trigger(markers[index], 'click');
 });
 
-//Event to filter places on change of search input
-$("body").on("change", "#filter-places", function(e){
-    e.preventDefault();
-    var value = $(this).val().toUpperCase();
-    var markersToHide = [];
-    var markersToShow = [];
-    
-    console.info('Filetering data for search criteria: ' + value );
-    if(value !==  ""){
-      $("#listing li").css("display", "block");
-          $("li").filter(":not(:contains('" + value + "'))").css("display", "none");
-    }else{
-      $("#listing li").css("display", "block");
-    }
-    //Get markers that are to be hidden!!
-    $("#listing li:not(:visible)").each(function(){
-        markersToHide.push(markers[$(this).data("marker-index")]);
-    });
-    //Get markers that are to be shown!!
-    $("#listing li:visible").each(function(){
-        markersToShow.push(markers[$(this).data("marker-index")]);
-    });
-    
-    hideMarkers(markersToHide);
-    showMarkers(markersToShow);
+$('body').on("showAllMyMarkersEvent", function(e){
+    showMarkers();
+});
+
+$('body').on("showMyMarkersEvent", function(e){
+    showMarkerAtIndex(e.index);
+});
+
+$('body').on("hideMyMarkersEvent", function(e){
+    hideMarkerAtIndex(e.index);
 });
