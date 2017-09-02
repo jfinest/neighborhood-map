0ef5ce27f593bb2ecf71a098423bcd105cc1dae0
JosueAcosta <josueacosta@IsidroskBookPro.home>
2017-09-02 00:35:37 -0400

    Modified file to have markers change color on mouseover.

--- js/script.js
+++ js/script.js
@@ -30,6 +30,8 @@ return locations;
 }
 
 function createMarkers(locations){
+  var defaultIcon = makeMarkerIcon('ff0000');
+  var highlightedIcon = makeMarkerIcon('FFFF24');
   // using locations to create marker's array  
   for (var i = 0; i < locations.length; i++){
     var position = locations[i].location;
@@ -42,6 +44,7 @@ function createMarkers(locations){
         position: position,
         title: title,
         animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
         id: id
     });
 
@@ -58,8 +61,27 @@ function createMarkers(locations){
       } else {
         getPlacesDetails(this, placeInfoWindow);
       }
-      });
+    });
+
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
   }
+}
+
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
 }
 
 //create listings
